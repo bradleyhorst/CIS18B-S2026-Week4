64ad0d0157fc28936b102e10cfe8b42a8e2f93e8
bradleyhorst <bhorst2@student.rccd.edu>
2026-03-24 02:42:41 +0000

    Completed mini08 observer pattern

--- week4-exercises/mini08-observer-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini08/AlertService.java
+++ week4-exercises/mini08-observer-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini08/AlertService.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AlertService {
+
     private final List<AlertObserver> observers = new ArrayList<>();
 
     public void addObserver(AlertObserver observer) {
-        // TODO: Add the observer to the list.
+        observers.add(observer);
     }
 
-    public void processAlert(Alert alert) {
-        System.out.println("Processing alert: " + alert.message());
-        // TODO: Notify all observers.
+    public void process(Alert alert) {
+
+        System.out.println("Processing alert: " + alert.getMessage());
+
+        notifyObservers(alert);
+    }
+
+    private void notifyObservers(Alert alert) {
+        for (AlertObserver observer : observers) {
+            observer.onAlert(alert);
+        }
     }
 }
