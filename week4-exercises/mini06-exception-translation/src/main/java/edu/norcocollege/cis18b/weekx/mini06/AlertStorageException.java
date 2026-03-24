f142b58c7767c23d3934e8833f917f4bdd97eb7b
bradleyhorst <bhorst2@student.rccd.edu>
2026-03-24 02:40:54 +0000

    Completed mini06 exception translation

--- week4-exercises/mini06-exception-translation/src/main/java/edu/norcocollege/cis18b/weekx/mini06/AlertStorageException.java
+++ week4-exercises/mini06-exception-translation/src/main/java/edu/norcocollege/cis18b/weekx/mini06/AlertStorageException.java
@@ -1,6 +1,11 @@
 package edu.norcocollege.cis18b.weekx.mini06;
 
 public class AlertStorageException extends Exception {
+
+    public AlertStorageException(String message) {
+        super(message);
+    }
+
     public AlertStorageException(String message, Throwable cause) {
         super(message, cause);
     }
