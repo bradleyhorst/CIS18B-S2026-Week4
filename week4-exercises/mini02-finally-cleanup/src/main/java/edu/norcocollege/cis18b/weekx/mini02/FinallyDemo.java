950afc6143ee6a4d9c6f736a8ca75d965dda1824
bradleyhorst <bhorst2@student.rccd.edu>
2026-03-24 02:34:58 +0000

    Completed mini02 finally cleanup

--- week4-exercises/mini02-finally-cleanup/src/main/java/edu/norcocollege/cis18b/weekx/mini02/FinallyDemo.java
+++ week4-exercises/mini02-finally-cleanup/src/main/java/edu/norcocollege/cis18b/weekx/mini02/FinallyDemo.java
@@ -1,15 +1,11 @@
-package edu.norcocollege.cis18b.weekx.mini02;
+public static void main(String[] args) {
+    System.out.println("Opening alert stream...");
 
-public class FinallyDemo {
-    public static void main(String[] args) {
-        // TODO: Print "Opening alert stream..."
-
-        try {
-            // TODO: Intentionally throw a RuntimeException with a helpful message.
-        } catch (RuntimeException ex) {
-            // TODO: Print the error message in a friendly format.
-        } finally {
-            // TODO: Print "Closing alert stream..."
-        }
+    try {
+        throw new RuntimeException("Something went wrong while processing alerts.");
+    } catch (RuntimeException ex) {
+        System.out.println("Error: " + ex.getMessage());
+    } finally {
+        System.out.println("Closing alert stream...");
     }
 }
