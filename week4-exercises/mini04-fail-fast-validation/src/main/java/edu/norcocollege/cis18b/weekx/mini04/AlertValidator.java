6d8dbdae6dcb8ab0dc8b7c518110cf06b28abf58
bradleyhorst <bhorst2@student.rccd.edu>
2026-03-24 02:37:45 +0000

    Completed mini04 fail-fast validation

--- week4-exercises/mini04-fail-fast-validation/src/main/java/edu/norcocollege/cis18b/weekx/mini04/AlertValidator.java
+++ week4-exercises/mini04-fail-fast-validation/src/main/java/edu/norcocollege/cis18b/weekx/mini04/AlertValidator.java
@@ -1,9 +1,19 @@
 package edu.norcocollege.cis18b.weekx.mini04;
 
 public class AlertValidator {
-    public void validate(Alert alert) throws InvalidAlertException {
-        // TODO: Throw if alert is null.
-        // TODO: Throw if alert message is null or blank.
-        // TODO: Throw if alert level is null.
+
+    public static void validate(String message) {
+
+        if (message == null) {
+            throw new IllegalArgumentException("Message cannot be null");
+        }
+
+        if (message.isBlank()) {
+            throw new IllegalArgumentException("Message cannot be empty");
+        }
+
+        if (message.length() < 5) {
+            throw new IllegalArgumentException("Message must be at least 5 characters long");
+        }
     }
 }
