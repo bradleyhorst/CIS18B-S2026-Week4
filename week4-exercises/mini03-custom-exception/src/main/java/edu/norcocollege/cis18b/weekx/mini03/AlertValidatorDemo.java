359ed6a31ee6eb49868a69ad7ca324ea9637ed3e
bradleyhorst <bhorst2@student.rccd.edu>
2026-03-24 02:36:52 +0000

    Completed mini03 custom exception

--- week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertValidatorDemo.java
+++ week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertValidatorDemo.java
@@ -2,19 +2,22 @@ package edu.norcocollege.cis18b.weekx.mini03;
 
 public class AlertValidatorDemo {
 
-    public static void validateAlert(Alert alert) throws InvalidAlertException {
-        // TODO: Throw InvalidAlertException if the message is null or blank.
-        // TODO: Throw InvalidAlertException if the level is null.
-    }
-
     public static void main(String[] args) {
-        Alert alert = new Alert(1, "", null);
+
+        String alert = ""; // invalid on purpose
 
         try {
             validateAlert(alert);
             System.out.println("Alert is valid.");
-        } catch (InvalidAlertException ex) {
-            System.out.println("Invalid alert: " + ex.getMessage());
+        } catch (InvalidAlertException e) {
+            System.out.println("Invalid alert: " + e.getMessage());
+        }
+    }
+
+    public static void validateAlert(String alert) throws InvalidAlertException {
+
+        if (alert == null || alert.isBlank()) {
+            throw new InvalidAlertException("Alert cannot be empty.");
         }
     }
 }
